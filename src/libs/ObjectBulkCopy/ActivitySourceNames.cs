976c1f1a2b343e83955a7629c33bac053e2aa8c3
Takaaki Suzuki <xin9le@live.jp>
2026-04-24 18:00:12 +0900

    Add ActivitySource caching mechanism

---
+++ src/libs/ObjectBulkCopy/ActivitySourceNames.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ObjectBulkCopy;
+
+
+
+/// <summary>
+/// Provides well-known activity source names used for tracing and diagnostics within the application.
+/// </summary>
+[Experimental("OBC001", UrlFormat = "https://github.com/dotnet/SqlClient/issues/2210#issuecomment-4281339991")]
+public static class ActivitySourceNames
+{
+    /// <summary>
+    /// Represents the default name.
+    /// </summary>
+    public const string Default = "ObjectBulkCopy";
+}
---
+++ src/libs/ObjectBulkCopy/Internals/ActivitySourceCache.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+namespace ObjectBulkCopy.Internals;
+
+
+
+/// <summary>
+/// Provides a caching mechanism for <see cref="ActivitySource"/>.
+/// </summary>
+internal static class ActivitySourceCache
+{
+    #region Fields
+#pragma warning disable OBC001
+    private static readonly ActivitySource s_activitySource = new(ActivitySourceNames.Default);
+#pragma warning restore OBC001
+    #endregion
+
+
+    #region Propeties
+    /// <summary>
+    /// Gets singleton instance.
+    /// </summary>
+    public static ActivitySource Instance
+        => s_activitySource;
+    #endregion
+
+
+    #region Constructors
+    /// <summary>
+    /// 
+    /// </summary>
+    static ActivitySourceCache()
+    {
+        var listener = new ActivityListener();
+        listener.ShouldListenTo += static src => src.Name == s_activitySource.Name;
+        listener.Sample += static (ref options) => ActivitySamplingResult.AllDataAndRecorded;
+        listener.SampleUsingParentId += static (ref options) => ActivitySamplingResult.AllDataAndRecorded;
+        ActivitySource.AddActivityListener(listener);
+    }
+    #endregion
+}
